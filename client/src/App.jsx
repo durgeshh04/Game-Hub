5ed93947a83ac13e6775c576fb031d16611f7051
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-08-30 01:30:47 +0530

    changes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import { Button } from "./components/ui/button";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import AuthLayout from "./components/auth/AuthLayout";
+import AdminLayout from "./components/admin-view/AdminLayout";
+import Dashboard from "./pages/admin-view/Dashboard";
+import Products from "./pages/admin-view/Products";
+import Orders from "./pages/admin-view/Orders";
+import Features from "./pages/admin-view/Features";
 
 export default function App() {
   return (
-    <>
-      <h1 className="font-bold text-4xl underline">Hello World</h1>
-    </>
-  )
+    <div>
+      {/* Common Components */}
+      <h1>Header Components</h1>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="features" element={<Features />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
