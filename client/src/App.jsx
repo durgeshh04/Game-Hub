964eb3d87cc8cd999a8a8e1a3bd3aa1aa45552a3
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-08-29 23:19:00 +0530

    update commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="font-bold text-4xl underline">Hello World</h1>
+    </>
+  )
+}
