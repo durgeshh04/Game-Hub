ebe2239ba4c490e89048c77311339d9c382b31fc
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-08-31 01:04:09 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,30 +13,56 @@ import ShoppingHome from "./pages/shopping-view/ShoppingHome";
 import ShoppingListing from "./pages/shopping-view/ShoppingListing";
 import ShoppingAccount from "./pages/shopping-view/ShoppingAccount";
 import ShoppingCheckout from "./pages/shopping-view/ShoppingCheckout";
+import CheckAuth from "./components/common/CheckAuth";
+import UnauthPage from "./pages/unauth-page/UnauthPage";
 
 export default function App() {
+  const isAuthenticated = false;
+  const user = null;
   return (
     <div>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="products" element={<Products />} />
           <Route path="orders" element={<Orders />} />
           <Route path="features" element={<Features />} />
         </Route>
 
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
         </Route>
 
+        <Route path="/unauth-page" element={<UnauthPage />} />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
